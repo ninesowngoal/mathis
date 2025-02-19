f9d6dc913e224b3dcf02dd552fdded5852df917b
ninesowngoal <c.chipeta1@gmail.com>
2025-02-19 11:07:53 +0000

    first commit

---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = () => {
+    return(
+        <div className="titlecontainer">
+            <h1 className="title">Calculate your way to victory.</h1>
+        </div>
+    );
+};
+
+export default Title
