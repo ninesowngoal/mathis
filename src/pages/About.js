2bb97e67e7491e0fb5457e14a1bf98657d2b7243
ninesowngoal <c.chipeta1@gmail.com>
2025-03-10 12:37:15 +0000

    Started About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 const About = () => {
   return (
-    <div>
-      <header>
-        <Header />
-      </header>
+    <div className='container'>
+      <Header />
+        <main className='main-content'>
+          <h2>ABOUT</h2>
+          <div className='about-box'>
+            <p className='about-text'>
+              Mathis is a website for Nikke players.
+            </p>
+          </div>
+        </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   )
 }
--- src/pages/Anomaly.js
+++ src/pages/Anomaly.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 const Anomaly = () => {
   return (
@@ -7,6 +8,9 @@ const Anomaly = () => {
       <header>
         <Header />
       </header>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   )
 }
